f5076aabb451fae8d8c0cf42e08bf656a5e065b0
harun alfarizi <mkydlffyy@gmail.com>
2026-08-22 11:02:59 +0700

    Menambahkan modul penyimpanan CSV dan logika pembuatan file

--- sistemWahana.py
+++ sistemWahana.py
@@ -24,3 +24,65 @@ wahana = {
         7: ("Kereta Mini", 10000)
     }
 }
+# BUAT FILE CSV JIKA BELUM ADA
+if not os.path.exists(FILE_CSV):
+    with open(FILE_CSV, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([
+            "Tanggal Pembelian",
+            "Nama",
+            "Tanggal Lahir",
+            "Alamat",
+            "Tiket Dewasa",
+            "Tiket Anak",
+            "Wahana",
+            "Potongan",
+            "Keterangan",
+            "Total Bayar"
+        ])
+
+# SIMPAN KE CSV
+def simpan_ke_csv(data):
+    with open(FILE_CSV, mode="a", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([
+            data["tanggal_pembelian"],
+            data["nama"],
+            data["tanggal_lahir"],
+            data["alamat"],
+            data["tiket_dewasa"],
+            data["tiket_anak"],
+            ", ".join(data["wahana"]),
+            data["potongan"],
+            data["keterangan"],
+            data["total_bayar"]
+        ])
+
+# BACA CSV
+def baca_csv():
+    data_pengunjung = []
+    with open(FILE_CSV, mode="r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data_pengunjung.append(row)
+    return data_pengunjung
+
+# UPDATE CSV
+def update_csv(data_pengunjung):
+    with open(FILE_CSV, mode="w", newline="") as file:
+        fieldnames = [
+            "Tanggal Pembelian",
+            "Nama",
+            "Tanggal Lahir",
+            "Alamat",
+            "Tiket Dewasa",
+            "Tiket Anak",
+            "Wahana",
+            "Potongan",
+            "Keterangan",
+            "Total Bayar"
+        ]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for data in data_pengunjung:
+            writer.writerow(data)
